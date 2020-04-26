d32010d65f0ccc53efa29d53bd854b2541de40f5
Ksen_Kas <q@o.ru>
2020-04-26 14:56:43 +0300

    Добавлена игра-рулетка - полная версия

--- ruletka.py
+++ ruletka.py
@@ -7,7 +7,7 @@ color_list = ['красное', 'черное', 'ЗЕРО']
 print(*color_list)
 print(*slot)
 
-#анализ
+#проверка
 bet_clr = 0
 a = 0
 while a != 1:
@@ -22,11 +22,11 @@ while a != 1:
             bet_clr = color_list[1]
             a = 1
             print('Играет черное!')
-    elif isinstance(bet, int):
-        if int(bet) in range(0, 37):
+    if isinstance(bet,int):
+        if bet in range(0, 37):
             a = 1
             print('Го3')
-
+#результат
 spin = random.choice(slot)
 i = spin
 clr = 5
@@ -42,7 +42,8 @@ elif i in range(11, 19) or i in range(29, 37):
         clr = color_list[0]
 else:
     clr = color_list[2]
-#результат
+
+#итог
 print('Выпало', end=' ')
 print(i, clr)
 
@@ -51,21 +52,3 @@ if spin == bet or bet_clr == clr:
     print('winner')
 else:
     print('loser')
-
-
-#
-# num = int(input("Под какое количество патронов обойма?: "))
-# real_bullets = int(input("Сколько патронов в обойме?: "))
-# bullets = [0 for i in range(num-real_bullets)]
-# bullets += [1 for j in range(real_bullets)]
-# print(bullets)
-#
-# a = input("Точно хочешь сыграть? Если да, то напиши 1, а если нет, то 0: ")
-#
-# while a == "1":
-#     if random.choice(bullets) == 1:
-#         print("Ты проиграл!!! Хочешь сыграть ещё раз?")
-#     else:
-#         print("Ты выиграл!!! Хочешь сыграть ещё раз?")
-#     a = input("Если да, то напиши 1, а если нет, то 0: ")
-# print("Спасибо за игру!")
