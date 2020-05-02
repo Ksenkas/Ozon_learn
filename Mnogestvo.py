714786e31ead08ed810ab46aad6968ea2da67274
Ksen_Kas <q@o.ru>
2020-05-02 20:40:41 +0300

    решение новое

--- Mnogestvo.py
+++ Mnogestvo.py
@@ -4,15 +4,17 @@
 # и множество людей, у которых и работа недалеко от мест преступления
 # имена обычно значения неуникальные, но предплоложим, это были бы номер соц страховок
 
-shevrole_owner = ['sam', 'edit', 'semen', 'petr']
-work_near = ['konstantin', 'vladislav', 'sam', 'petr', 'edit']
-live_near = ['john', 'vladislav', 'olga', 'mike', 'grant', 'covid', 'bilbo']
-suspect = []
+shevrole_owner = {'sam', 'edit', 'semen', 'petr'}
+work_near = {'konstantin', 'vladislav', 'sam', 'petr', 'edit'}
+live_near = {'john', 'vladislav', 'olga', 'mike', 'grant', 'covid', 'bilbo'}
 
-#убрали дубли из 2 списков
-suspect.extend(work_near + live_near)
-suspect = list(dict.fromkeys(suspect))
-print(suspect)
+
+# убрали дубли из 2 списков
+suspect = live_near.union(work_near)
+
+# найдем подозреваемых
+suspect_car = shevrole_owner.intersection(suspect)
+print(*ssuspect_car)
 
 
 
